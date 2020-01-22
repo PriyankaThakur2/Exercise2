1c58f6d0903cb0413cb9d8825eb2c24af3e668c8
Priyanka Thakur <thakurp@usf.edu>
2020-01-21 21:01:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 26;
+            int height = 61;
+
+           //Max heartrate is equal to 220 - age
+
+            int heartrate = 220 - age;
+
+            // COnsole should write : I am ## years old, my max heartrate is ##
+            // I am ## cms tall
+
+
+            Console.WriteLine("I am " + age + " years old, my max heartrate is " + heartrate + " and I am " + height +
+                "cms tall");
+        }
+    }
+}
